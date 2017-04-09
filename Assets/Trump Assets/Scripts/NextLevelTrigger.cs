088c9d7e346c307b938043dd4fbd6289dba6947c
WINDOWS-EFH65RO\Thomas <ttattr-u9reweup9]>
2017-04-09 17:18:05 -0500

    no message

---
+++ Assets/Trump Assets/Scripts/NextLevelTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelTrigger : MonoBehaviour {
+
+    public string levelName = "DC Level";
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(levelName);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
